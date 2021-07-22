13d8788876dd12a5ea151780a3a10b4c7f46d9bb
BigBodyBugatti <84071231+BigBodyBugatti@users.noreply.github.com>
2021-07-22 11:56:21 +0200

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.lang.*;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        String str = " Hello World! ";
+        str = str.toLowerCase();
+        System.out.println(str);
+
+        str = str.toUpperCase();
+        System.out.println(str);
+
+
+        int i=0;
+        do{
+            System.out.println("Hello World " +i);
+            i++;
+        }
+        while(i<=15);
+
+        String s = "Hello World! ";
+        System.out.println("String s = " + s);
+        String s1 = new String("Hello Codersbay!");
+        System.out.println("String s1 = " + s1);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
